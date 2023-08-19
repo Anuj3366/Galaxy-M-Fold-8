c089584fe95c6f32ee5474cdfc3a75bd2b0e2e5c
Anuj Garg <anujgarg3366@gmail.com>
2023-08-20 00:20:07 +0530

    made some basic structure and done 1/6 of work

--- Task 1/script.js
+++ Task 1/script.js
@@ -3,20 +3,19 @@ function showNewsletterPopup() {
     overlay.style.display = 'flex';
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const overlay = document.getElementById('overlay');
     const popup = document.getElementById('popup');
 
-    overlay.addEventListener('click', function(event) {
+    overlay.addEventListener('click', function (event) {
         if (event.target === overlay) {
             overlay.style.display = 'none';
         }
     });
 
-    popup.addEventListener('submit', function(event) {
+    popup.addEventListener('submit', function (event) {
         event.preventDefault();
         const email = document.getElementById('email').value;
-        // Process the subscription here (e.g., send to server, show confirmation, etc.)
         overlay.style.display = 'none';
     });
 });
